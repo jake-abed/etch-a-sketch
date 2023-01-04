a43313b5701e6493aed9dac497583693acca11cf
jake-abed <jakeabed@gmail.com>
2023-01-03 21:33:39 -0800

    Add settings UI, Reset, Fill Modes

--- scripts/etch-a-sketch.js
+++ scripts/etch-a-sketch.js
@@ -1,15 +1,28 @@
 'use strict';
+//Get Random number helper function
+const randomNumberUpTo = (maxVal) => Math.floor(Math.random()*maxVal+1);
 function allBlackFill() {
-	return this.setAttribute('style', 'background-color: black');
+	return 'background-color: black';
+}
+function randomColorFill() {
+	const redVal = randomNumberUpTo(256);
+	const greenVal = randomNumberUpTo(256);
+	const blueVal = randomNumberUpTo(256);
+	return  `background-color: rgb(${redVal}, ${greenVal}, ${blueVal});`;
+}
+function fillPixel() {
+	return this.setAttribute('style', settings.mode());
 }
 //Declare settings object, with 'mode' property and 'squaresPerSide'
 const settings = {
-	mode: allBlackFill,
+	mode: randomColorFill,
 	squaresPerSide: 16
 }
 //Declare variables to store DOM nodes.
 const screenNode = document.querySelector('.etch-a-sketch-screen'),
-	resetButton = document.querySelector('.reset-button');
+	resetButton = document.querySelector('.reset'),
+	allBlackButton = document.querySelector('.all-black'),
+	rainbowFillButton = document.querySelector('.rainbow-fill');
 //Fill .etch-a-sketch-screen div with X * X square divs, X being settings.squaresPerSide
 const fillScreen = (sideLength) => {
 	const TOTAL_DIVS = sideLength * sideLength;
@@ -18,13 +31,24 @@ const fillScreen = (sideLength) => {
 		const pixelDiv = document.createElement('div');
 		pixelDiv.setAttribute('id', `div-${i}`);
 		pixelDiv.setAttribute('class', 'pixel-div');
-		pixelDiv.addEventListener('pointerover', settings.mode);
-		pixelDiv.addEventListener('touch', settings.mode);
+		pixelDiv.addEventListener('pointerenter', fillPixel);
 		screenNode.append(pixelDiv);
 	}
 }
-//Assign 'hover' event listeners to each pixel that run a fill function, settings.mode's current assignment.
-//When user clicks reset function, the divs are deleted and the etch-a-sketch reinits per specs.
 
+//swap modes helper function
+const swapFillMode = (mode) => {
+	settings.previousMode = settings.mode;
+	settings.mode = mode;
+}
+//When user clicks reset function, the divs are deleted and the etch-a-sketch reinits per specs.
+const resetScreen = () => {
+	document.querySelectorAll('.pixel-div').forEach(pixel => pixel.remove());
+	fillScreen(settings.squaresPerSide);
+	return console.log('Screen Reset');
+}
 //finally, init the etch-a-sketch toy
+allBlackButton.addEventListener('pointerdown', () => swapFillMode(allBlackFill));
+rainbowFillButton.addEventListener('pointerdown', () => swapFillMode(randomColorFill));
+resetButton.addEventListener('pointerdown', resetScreen);
 fillScreen(settings.squaresPerSide);
