778e47277cb5970db50b519da7e14d3af490d118
Jake Abed <jakeabed@gmail.com>
2023-01-05 15:31:57 -0800

    Update fill events + add basic resize function

--- scripts/etch-a-sketch.js
+++ scripts/etch-a-sketch.js
@@ -1,17 +1,15 @@
 'use strict';
 //Get Random number helper function
 const randomNumberUpTo = (maxVal) => Math.floor(Math.random()*maxVal+1);
-function allBlackFill() {
-	return 'background-color: black';
-}
+const allBlackFill = () => 'background-color: black';
 function randomColorFill() {
 	const redVal = randomNumberUpTo(256);
 	const greenVal = randomNumberUpTo(256);
 	const blueVal = randomNumberUpTo(256);
 	return  `background-color: rgb(${redVal}, ${greenVal}, ${blueVal});`;
 }
-function fillPixel() {
-	return this.setAttribute('style', settings.mode());
+const fillPixel = (pixel) => {
+	return pixel.setAttribute('style', settings.mode());
 }
 //Declare settings object, with 'mode' property and 'squaresPerSide'
 const settings = {
@@ -21,6 +19,7 @@ const settings = {
 //Declare variables to store DOM nodes.
 const screenNode = document.querySelector('.etch-a-sketch-screen'),
 	resetButton = document.querySelector('.reset'),
+	resizeButton = document.querySelector('.resize'),
 	allBlackButton = document.querySelector('.all-black'),
 	rainbowFillButton = document.querySelector('.rainbow-fill');
 //Fill .etch-a-sketch-screen div with X * X square divs, X being settings.squaresPerSide
@@ -31,11 +30,12 @@ const fillScreen = (sideLength) => {
 		const pixelDiv = document.createElement('div');
 		pixelDiv.setAttribute('id', `div-${i}`);
 		pixelDiv.setAttribute('class', 'pixel-div');
-		pixelDiv.addEventListener('pointerenter', fillPixel);
+		pixelDiv.addEventListener('pointerenter', (event) => {
+			if (event.buttons == 1) fillPixel(pixelDiv);
+		});
 		screenNode.append(pixelDiv);
 	}
 }
-
 //swap modes helper function
 const swapFillMode = (mode) => {
 	settings.previousMode = settings.mode;
@@ -47,8 +47,18 @@ const resetScreen = () => {
 	fillScreen(settings.squaresPerSide);
 	return console.log('Screen Reset');
 }
+//Helper function for user input to change screen pixel density
+const resizeScreen = () => {
+	settings.squaresPerSide = prompt('How many pixels would you like per side? Please input a number less than or equal to 64.', 16).toString();
+	if (settings.squaresPerSide > 64) {
+		alert('Can you read? I said less than or equal to 64.');
+		settings.squaresPerSide = 64;
+	}
+	resetScreen();
+}
 //finally, init the etch-a-sketch toy
 allBlackButton.addEventListener('pointerdown', () => swapFillMode(allBlackFill));
 rainbowFillButton.addEventListener('pointerdown', () => swapFillMode(randomColorFill));
 resetButton.addEventListener('pointerdown', resetScreen);
+resizeButton.addEventListener('pointerdown', resizeScreen);
 fillScreen(settings.squaresPerSide);
